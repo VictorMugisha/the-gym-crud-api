2f3484669436dc8c1868aeff124e52c68d44e1de
Victor Mugisha Shyaka <victormugishavm6@gmail.com>
2024-09-17 19:32:07 +0200

    Create a model for items

---
+++ models/item.model.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const itemSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ItemModel = mongoose.model("Item", itemSchema);
+
+module.exports = ItemModel;
